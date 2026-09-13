05f7d3c3da41278b94d00a97afb35efb1be9fb7e
ahmet <ahmet@SAMSUNG>
2026-09-13 16:21:42 +0300

    Proje dosyası ekle.

---
+++ student-friendly/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Enter the two exam scores: ");
+
+short score1 = Convert.ToInt16(Console.ReadLine());
+short score2 = Convert.ToInt16(Console.ReadLine());
+float average = (score1 + score2) / 2.0f;
+Console.WriteLine("Bro you scored " + average);
+if (average > 50)
+{
+    Console.WriteLine("Bro passed school!");
+}
+else
+{
+    Console.WriteLine("R.İ.P(bro not passed school called mom)");
+}
